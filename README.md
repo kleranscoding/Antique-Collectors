1f800f542798af7b0f9ae6e59b274fc2d7f202f6
kleranscoding <clarencecc88@gmail.com>
2019-01-19 20:16:06 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Antique Collectors
+## Collabathon project with UXDesigners (Jan 17-18, 2019)
---
+++ public/scripts/index.js
@@ -0,0 +1,25 @@
+
+const timeout= 5000;
+var modalSignup = document.getElementsByClassName("modal-signup")[0];
+var closeModalSignup = document.getElementsByClassName("close-modal-signup")[0];
+var subscribeBtn = document.getElementById("to_subscribe");
+
+window.addEventListener("click",function(event){
+    if (event.target.className==="modal-signup") {
+        modalSignup.style.display= "none";
+    }
+})
+
+window.onload=function(){
+    setTimeout(function(){ 
+        modalSignup.style.display= "block"; 
+    }, timeout);
+};
+
+closeModalSignup.addEventListener('click',function(){
+    modalSignup.style.display= "none";
+});
+
+subscribeBtn.addEventListener('click',function(){
+    window.location.href="/subscribe";
+});
